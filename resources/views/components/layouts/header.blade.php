7139454acb591a704f5c3964ef4a9d5ba2c18bcf
jison-jpeg <2101101090@student.buksu.edu.ph>
2024-02-02 00:14:21 +0800

    Update padding-left in header

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,10 +1,8 @@
-
 <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
+        <a href="index.html" class="logo d-flex align-items-center d-none d-lg-block">
             <img src="assets/img/logo.png" alt="">
-            <!-- <span class="d-none d-lg-block">ormy</span> -->
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
     </div>
@@ -214,13 +212,13 @@
                     </li>
 
                     <li>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit" class="dropdown-item d-flex align-items-center">
-                            <i class="bi bi-box-arrow-right"></i>
-                            <span>Sign Out</span>
-                        </button>
-                    </form>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="dropdown-item d-flex align-items-center">
+                                <i class="bi bi-box-arrow-right"></i>
+                                <span>Sign Out</span>
+                            </button>
+                        </form>
                     </li>
 
                 </ul><!-- End Profile Dropdown Items -->
